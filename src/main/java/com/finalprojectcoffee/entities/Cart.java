22a80b8dd0a5c17b0589005637778e0be59ccd3c
CheePheng <wildwoodywildwoody@outlook.com>
2024-02-05 11:16:01 +0000

    Cart and review entities

---
+++ src/main/java/com/finalprojectcoffee/entities/Cart.java
@@ -0,0 +1,81 @@
+package com.finalprojectcoffee.entities;
+
+import jakarta.persistence.Table;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "cart")
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "user_id", nullable = false)
+    private Long userId;
+
+    @Column(name = "order_id", nullable = false)
+    private Long orderId;
+
+    @Column(name = "cost", nullable = false)
+    private Double cost;
+
+    @Column(name = "quantity", nullable = false)
+    private Integer quantity;
+
+
+
+    public Cart() {
+    }
+
+    public Cart(Long userId, Long orderId, Double cost, Integer quantity) {
+        this.userId = userId;
+        this.orderId = orderId;
+        this.cost = cost;
+        this.quantity = quantity;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public Double getCost() {
+        return cost;
+    }
+
+    public void setCost(Double cost) {
+        this.cost = cost;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+}
+
+
