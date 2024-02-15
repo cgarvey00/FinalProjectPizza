6d4ba46e4c41a39b67dea2f60da1219faca1ce70
Tom <tomzhou666@outlook.com>
2024-02-15 11:55:29 +0000

    Corrected Email regular expression.

--- src/main/java/com/finalprojectcoffee/utils/EmailUtil.java
+++ src/main/java/com/finalprojectcoffee/utils/EmailUtil.java
@@ -4,7 +4,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class EmailUtil {
-    private static final Pattern VALID_EMAIL_ADDRESS_REGEX = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+    private static final Pattern VALID_EMAIL_ADDRESS_REGEX = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
     public static Boolean validateEmail(String email){
         Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(email);
         return matcher.matches();
